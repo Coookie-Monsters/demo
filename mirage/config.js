38367d91a6364a8e04d49ce89fba151baaac1777
David Merrick <david@david-merrick.com>
2019-01-12 11:58:23 -0800

    Changed categories

--- mirage/config.js
+++ mirage/config.js
@@ -12,41 +12,26 @@ export default function() {
   let rentals = [
     {
       type: 'rentals',
-      id: 'grand-old-mansion',
+      id: 'tools',
       attributes: {
-        title: "Grand Old Mansion",
-        owner: "Veruca Salt",
-        city: "San Francisco",
-        category: "Estate",
-        bedrooms: 15,
-        image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg",
-        description: "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
+        title: "Tools",
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
       }
     },
     {
       type: 'rentals',
-      id: 'urban-living',
+      id: 'seeds',
       attributes: {
-        title: "Urban Living",
-        owner: "Mike Teavee",
-        city: "Seattle",
-        category: "Condo",
-        bedrooms: 1,
-        image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg",
-        description: "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
+        title: "Seeds",
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
       }
     },
     {
       type: 'rentals',
-      id: 'downtown-charm',
+      id: 'plants',
       attributes: {
-        title: "Downtown Charm",
-        owner: "Violet Beauregarde",
-        city: "Portland",
-        category: "Apartment",
-        bedrooms: 3,
-        image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg",
-        description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
+        title: "Plants",
+        image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
       }
     }
   ];
