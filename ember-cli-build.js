356ecaf086b51b9adc915e56f1676dc7f95c6b13
David Merrick <david@david-merrick.com>
2019-01-12 12:17:39 -0800

    Added Bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
+      'importBootstrapCSS': true,
+      whitelist: ['bs-button']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
