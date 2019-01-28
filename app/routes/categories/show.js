13fc717d9c2da5757f78e121bb063f9fe9eb2157
Abbie Glunn <aglunn@gmail.com>
2019-01-27 23:06:02 -0800

    Add donation model and functional dropdown

--- app/routes/categories/show.js
+++ app/routes/categories/show.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default Route.extend({
   model(params) {
-    return this.store.findRecord('category', params.category_id);
+    
+    return hash({
+      donation: this.store.createRecord('donation'),
+      category: this.store.findRecord('category', params.category_id)
+    });
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -59,6 +59,11 @@ export default function () {
     return {data: categories.find((category) => request.params.id === category.id)};
   });
 
+  this.post('/donations', (schema, request) => {
+    const attrs = JSON.parse(request.requestBody).donation;
+
+    return schema.donations.create(attrs);
+  });
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
