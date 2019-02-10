9f556127fd0df8beffa99613fdb7d4615b3c0e08
David Merrick <david@david-merrick.com>
2019-02-09 17:25:57 -0800

    Got bootstrap working with sass (I think)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var nodeSass = require('node-sass');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     'ember-bootstrap': {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
-      'importBootstrapCSS': true
+      'importBootstrapCSS': false
     },
-    'sassOptions': {
-      'implementation': 'nodeSass'
+    sassOptions: {
+      implementation: nodeSass
     }
   });
 
