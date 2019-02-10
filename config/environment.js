ca59378661f7256f0a86f87d51736b48abc15deb
David Merrick <david@david-merrick.com>
2019-02-09 17:58:46 -0800

    Got Bootstrap working (#10)
    * WIP: getting SASS working so we can integrate Bootstrap
    
    * Got bootstrap working with sass (I think)
    
    * Imported Bootstrap SASS and got it working on the category page

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'super-rentals',
+    modulePrefix: 'growing-gardens',
     environment,
     rootURL: '/',
     locationType: 'auto',
@@ -22,14 +22,6 @@ module.exports = function(environment) {
       // when it is created
     }
   };
-
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
 
   if (environment === 'test') {
     // Testem prefers this...
